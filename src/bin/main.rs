5374dd9a452cd7b697c6915a82372a9b16aad5be
Shane Sniff <slsniff@gmail.com>
2021-03-03 17:51:27 -0800

    update main and rerun

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,6 +6,9 @@ use std::path::Path;
 use indicatif::ProgressBar;
 use png;
 
+use trace::color::write_color;
+use trace::vec3::Color;
+
 fn main() {
     let width = 600;
     let height = 400;
@@ -15,19 +18,18 @@ fn main() {
     let mut data = Vec::new();
     for j in (0..height).rev() {
         for i in 0..width {
-            let r = i as f64 / width as f64;
-            let g = j as f64 / height as f64;
-            let b = 0.25;
-            let ir = (255.99 * r as f64) as u8;
-            let ig = (255.99 * g as f64) as u8;
-            let ib = (255.99 * b as f64) as u8;
-            data.extend_from_slice(&[ir, ig, ib, 255]);
+            let pixel_color = Color::new(
+                i as f64 / (width - 1) as f64,
+                j as f64 / (height - 1) as f64,
+                0.25,
+            );
+            write_color(&mut data, pixel_color);
             bar.inc(1);
         }
     }
     bar.finish();
 
-    let path = Path::new(r"./images/chapter-2.png");
+    let path = Path::new(r"./images/chapter-3.png");
     let file = File::create(path).unwrap();
     let w = BufWriter::new(file);
 
