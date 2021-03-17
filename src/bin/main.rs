fe57feba362f6e346c86cb8debb79f55273140cb
Shane Sniff <slsniff@gmail.com>
2021-03-16 22:15:07 -0700

    add ray implementation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,29 +7,51 @@ use indicatif::ProgressBar;
 use png;
 
 use trace::color::write_color;
-use trace::vec3::Color;
+use trace::ray::Ray;
+use trace::vec3::{unit_vector, Color, Point3, Vec3};
+
+fn ray_color(r: Ray) -> Color {
+    let unit_direction = unit_vector(r.direction);
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
+    // image
+    let aspect_ratio = 16.0 / 9.0;
     let width = 600;
-    let height = 400;
+    let height = (width as f64 / aspect_ratio) as u32;
+
+    // camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
 
     let bar = ProgressBar::new((width * height) as u64);
 
     let mut data = Vec::new();
     for j in (0..height).rev() {
         for i in 0..width {
-            let pixel_color = Color::new(
-                i as f64 / (width - 1) as f64,
-                j as f64 / (height - 1) as f64,
-                0.25,
+            let u = i as f64 / (width - 1) as f64;
+            let v = j as f64 / (height - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray_color(r);
             write_color(&mut data, pixel_color);
             bar.inc(1);
         }
     }
     bar.finish();
 
-    let path = Path::new(r"./images/chapter-3.png");
+    let path = Path::new(r"./images/chapter-4.png");
     let file = File::create(path).unwrap();
     let w = BufWriter::new(file);
 
