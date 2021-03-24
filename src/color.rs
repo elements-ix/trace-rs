a533d1c95640ce7bedf1038c64e19c592050a10a
Shane Sniff <slsniff@gmail.com>
2021-03-23 21:03:46 -0700

    add clamp and update write_color

--- src/color.rs
+++ src/color.rs
@@ -1,10 +1,15 @@
-use crate::vec3::Color;
+use crate::{util::clamp, vec3::Color};
 
-pub fn write_color(data: &mut Vec<u8>, pixel_color: Color) {
+pub fn write_color(data: &mut Vec<u8>, pixel_color: Color, samples_per_pixel: u32) {
+    let scale = 1.0 / samples_per_pixel as f64;
+    let r = pixel_color.x() * scale;
+    let g = pixel_color.y() * scale;
+    let b = pixel_color.z() * scale;
+
     data.extend_from_slice(&[
-        (255.99 * pixel_color.x()) as u8,
-        (255.99 * pixel_color.y()) as u8,
-        (255.99 * pixel_color.z()) as u8,
+        (255.99 * clamp(r, 0.0, 0.999)) as u8,
+        (255.99 * clamp(g, 0.0, 0.999)) as u8,
+        (255.99 * clamp(b, 0.0, 0.999)) as u8,
         255,
     ]);
 }
--- src/util.rs
+++ src/util.rs
@@ -3,3 +3,13 @@ use std::f64::consts::PI;
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180.0
 }
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        return min;
+    }
+    if x > max {
+        return max;
+    }
+    x
+}
