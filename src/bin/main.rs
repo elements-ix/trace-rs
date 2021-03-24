34beac4ce45a8a9bcbd5fcfecace3e1614513aec
Shane Sniff <slsniff@gmail.com>
2021-03-23 21:03:55 -0700

    add rand and update main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,12 +5,13 @@ use std::path::Path;
 
 use indicatif::ProgressBar;
 use png;
+use rand::prelude::*;
 
-use trace::color::write_color;
 use trace::hit::Hit;
 use trace::ray::Ray;
 use trace::sphere::Sphere;
-use trace::vec3::{unit_vector, Color, Point3, Vec3};
+use trace::vec3::{unit_vector, Color, Point3};
+use trace::{camera::Camera, color::write_color};
 
 fn ray_color(r: &Ray, world: &dyn Hit) -> Color {
     if let Some(hit) = world.hit(r, 0.001, f64::INFINITY) {
@@ -23,10 +24,13 @@ fn ray_color(r: &Ray, world: &dyn Hit) -> Color {
 }
 
 fn main() {
+    let mut rng = thread_rng();
+
     // image
     let aspect_ratio = 16.0 / 9.0;
     let width = 600;
     let height = (width as f64 / aspect_ratio) as u32;
+    let samples_per_pixel = 100;
 
     // world
     let world: Vec<Box<dyn Hit>> = vec![
@@ -35,35 +39,27 @@ fn main() {
     ];
 
     // camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
+    let cam = Camera::new();
 
     let bar = ProgressBar::new((width * height) as u64);
 
     let mut data = Vec::new();
     for j in (0..height).rev() {
         for i in 0..width {
-            let u = i as f64 / (width - 1) as f64;
-            let v = j as f64 / (height - 1) as f64;
-            let r = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
-            let pixel_color = ray_color(&r, &world);
-            write_color(&mut data, pixel_color);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for s in 0..samples_per_pixel {
+                let u = (i as f64 + rng.gen::<f64>()) / (width - 1) as f64;
+                let v = (j as f64 + rng.gen::<f64>()) / (height - 1) as f64;
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
+            write_color(&mut data, pixel_color, samples_per_pixel);
             bar.inc(1);
         }
     }
     bar.finish();
 
-    let path = Path::new(r"./images/chapter-6-2.png");
+    let path = Path::new(r"./images/chapter-7.png");
     let file = File::create(path).unwrap();
     let w = BufWriter::new(file);
 
