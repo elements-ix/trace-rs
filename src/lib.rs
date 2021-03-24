72901d1c512fdb671efc124a2b1e7dd0331f2fe4
Shane Sniff <slsniff@gmail.com>
2021-03-23 21:03:25 -0700

    add camera class

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
+pub mod camera;
 pub mod color;
 pub mod hit;
 pub mod ray;
 pub mod sphere;
 pub mod util;
 pub mod vec3;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
