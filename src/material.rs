714715deae858e620864cfba00eccc77eb528456
Shane Sniff <slsniff@gmail.com>
2021-03-28 10:39:21 -0700

    add dielectric material

--- src/material.rs
+++ src/material.rs
@@ -1,7 +1,9 @@
+use rand::prelude::*;
+
 use crate::{
     hit::HitRecord,
     ray::Ray,
-    vec3::{dot, random_in_unit_sphere, random_unit_vector, reflect, unit_vector, Color},
+    vec3::{dot, random_in_unit_sphere, random_unit_vector, reflect, refract, unit_vector, Color},
 };
 
 pub struct ScatteredRay {
@@ -45,4 +47,52 @@ impl Scatter for Metal {
 
         None
     }
+}
+
+pub struct Dielectric {
+    pub ir: f64,
+}
+
+impl Scatter for Dielectric {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<ScatteredRay> {
+        let mut rng = thread_rng();
+
+        let attenuation = Color::new(1.0, 1.0, 1.0);
+        let etai_over_etat = if rec.front_face {
+            1.0 / self.ir
+        } else {
+            self.ir
+        };
+
+        let unit_direction = unit_vector(r_in.direction);
+
+        let cos_theta = dot(&(-1.0 * unit_direction), &rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+        let reflected = reflect(unit_direction, rec.normal);
+        if (etai_over_etat * sin_theta) > 1.0 {
+            return Some(ScatteredRay {
+                attenuation,
+                scattered: Ray::new(rec.p, reflected),
+            });
+        }
+
+        let reflect_prob = schlick(cos_theta, etai_over_etat);
+        if rng.gen::<f64>() < reflect_prob {
+            return Some(ScatteredRay {
+                attenuation,
+                scattered: Ray::new(rec.p, reflected),
+            });
+        }
+        let refracted = refract(unit_direction, rec.normal, etai_over_etat);
+        Some(ScatteredRay {
+            attenuation,
+            scattered: Ray::new(rec.p, refracted),
+        })
+    }
+}
+
+pub fn schlick(cosine: f64, ref_idx: f64) -> f64 {
+    let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    r0 = r0 * r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
 }
